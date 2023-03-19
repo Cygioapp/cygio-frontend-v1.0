a71446d59e7ea4602ac66f73f46723a53db43518
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2023-03-19 22:49:48 +0100

    save

---
+++ pages/Auth/Signup.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <div>
+      sign up
+      <Link href='/'>go to login</Link>
+
+    </div>
+  )
+}
