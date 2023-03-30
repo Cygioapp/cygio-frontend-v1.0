48efc9a0bc3551ce8f64828c8d83a7b39dd25fea
Hussaini Usman <hussainmaina27@gmail.com>
2023-03-30 15:29:13 +0100

    add login functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/components/styles/globals.css'
+import "@/components/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
